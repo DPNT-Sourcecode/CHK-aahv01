dc9624c87aa1b5dbe41867c2958a10141c86067a
sbx_user1051 <sbx_user1051@169.254.111.165>
2024-07-20 17:04:18 +0000

    Sat Jul 20 17:04:18 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,18 +7,57 @@ import java.util.Map;
 
 public class CheckoutSolution {
 
-    final int priceA = 50;
-    final int priceB = 30;
-    final int priceC = 20;
-    final int priceD = 15;
-    final int priceE = 40;
-    final int priceF = 10;
-    final int specialPriceA3 = 130;
-    final int specialCountA3 = 3;
-    final int specialPriceA5 = 200;
-    final int specialCountA5 = 5;
-    final int specialPriceB = 45;
-    final int specialCountB = 2;
+    private static final Map<Character, Product> products = new HashMap<>();
+
+    {
+        products.put('A', new Product(50));
+        products.get('A').addSpecialOffer(5, 200);
+        products.get('A').addSpecialOffer(3, 130);
+
+        products.put('B', new Product(30));
+        products.get('B').addSpecialOffer(2, 45);
+
+        products.put('C', new Product(20));
+        products.put('D', new Product(15));
+        products.put('E', new Product(40));
+        products.put('F', new Product(10));
+        products.put('G', new Product(20));
+
+        products.put('H', new Product(10));
+        products.get('H').addSpecialOffer(10, 80);
+        products.get('H').addSpecialOffer(5, 45);
+
+        products.put('I', new Product(35));
+        products.put('J', new Product(60));
+
+        products.put('K', new Product(80));
+        products.get('K').addSpecialOffer(2, 150);
+
+        products.put('L', new Product(90));
+        products.put('M', new Product(15));
+        products.put('N', new Product(40));
+        products.put('O', new Product(10));
+
+        products.put('P', new Product(50));
+        products.get('P').addSpecialOffer(5, 200);
+
+        products.put('Q', new Product(30));
+        products.get('Q').addSpecialOffer(3, 80);
+
+        products.put('R', new Product(50));
+        products.put('S', new Product(30));
+        products.put('T', new Product(20));
+        products.put('U', new Product(40));
+
+        products.put('V', new Product(50));
+        products.get('V').addSpecialOffer(3, 130);
+        products.get('V').addSpecialOffer(2, 90);
+
+        products.put('W', new Product(20));
+        products.put('X', new Product(90));
+        products.put('Y', new Product(10));
+        products.put('Z', new Product(50));
+    }
 
     public Integer checkout(String skus) {
         Map<Character, Integer> itemCount = new HashMap<>();
@@ -79,8 +118,23 @@ public class CheckoutSolution {
         return total;
     }
 
+    class Product {
+        int price;
+        Map<Integer, Integer> specialOffers;
+
+        public Product(int price) {
+            this.price = price;
+            this.specialOffers = new HashMap<>();
+        }
+
+        public void addSpecialOffer(int count, int price) {
+            specialOffers.put(count, price);
+        }
+    }
+
     public static void main(String[] args) {
         CheckoutSolution checkoutSolution = new CheckoutSolution();
         System.out.println(checkoutSolution.checkout("FFF"));
     }
 }
+
